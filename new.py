7d1bc42bb1df930a70f99964337ea00daf8b3cba
Yash Badgujar <128134326+MrRobot373@users.noreply.github.com>
2025-05-07 15:35:21 +0530

    Update new.py

--- new.py
+++ new.py
@@ -26,7 +26,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-8b",
+    model_name="gemini-2.0-flash-lite",
     generation_config=generation_config,
 )
 
